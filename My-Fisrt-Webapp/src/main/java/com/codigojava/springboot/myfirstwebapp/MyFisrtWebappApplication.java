5099fb5000c0a81d5825654b4313b13065dd868c
Cyb3rh4ck <cyb3rh4ck@outlook.com>
2025-01-28 23:27:29 -0600

    My first webapp

---
+++ My-Fisrt-Webapp/src/main/java/com/codigojava/springboot/myfirstwebapp/MyFisrtWebappApplication.java
@@ -0,0 +1,13 @@
+package com.codigojava.springboot.myfirstwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFisrtWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFisrtWebappApplication.class, args);
+	}
+
+}
---
+++ My-Fisrt-Webapp/src/main/java/com/codigojava/springboot/myfirstwebapp/hello/controllers/SayHelloController.java
@@ -0,0 +1,33 @@
+package com.codigojava.springboot.myfirstwebapp.hello.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+	
+	@GetMapping("say-hello")
+	@ResponseBody
+	public String sayHello() {
+		return "Hello! What are you learning today?";
+	}
+	
+	
+	@GetMapping("say-hello-html")
+	@ResponseBody
+	public String sayHelloHtml() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("<html>");
+		sb.append("<head>");
+		sb.append("<title>My first HTML Page</title>");
+		sb.append("</head>");
+		sb.append("<body>");
+		sb.append("My first HTML with body");
+		sb.append("</body>");
+		sb.append("</html>");
+		
+		return sb.toString();
+	}
+
+}
