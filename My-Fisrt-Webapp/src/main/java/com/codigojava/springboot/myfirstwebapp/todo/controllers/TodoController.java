3803ce1475b63d5c96e9842395bed4145c55fdf6
Efren Hernandez <efren.hernandez@outlook.com>
2025-02-09 16:02:12 -0600

    validations

--- My-Fisrt-Webapp/src/main/java/com/codigojava/springboot/myfirstwebapp/todo/controllers/TodoController.java
+++ My-Fisrt-Webapp/src/main/java/com/codigojava/springboot/myfirstwebapp/todo/controllers/TodoController.java
@@ -1,14 +1,16 @@
 package com.codigojava.springboot.myfirstwebapp.todo.controllers;
 
+
 import java.time.LocalDate;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.codigojava.springboot.myfirstwebapp.todo.models.Todo;
 import com.codigojava.springboot.myfirstwebapp.todo.services.TodoService;
 
 @Controller
@@ -22,19 +24,22 @@ public class TodoController {
 	}
 	
 	@GetMapping("list-todos")
-	public String listAllTodos(Model model) {
+	public String listAllTodos(ModelMap model) {
 		model.addAttribute("todos",todoService.findByUsername("CodigoJava"));
 		return "listTodos";
 	}
 	
 	@GetMapping("add-todo")
-	public String showNewTodoPage() {
+	public String showNewTodoPage(ModelMap model) {
+		String username = (String)model.getAttribute("username");
+		Todo todo = new Todo(0, username, "Defatul Desc", LocalDate.now().plusYears(1), false);
+		model.put("todo", todo);
 		return "todo";
 	}
 	
 	@PostMapping("add-todo")
-	public String addNewTodo(@RequestParam String description, Model model) {
-		todoService.addTodo(model.getAttribute("name").toString(), description, LocalDate.now().plusMonths(8), false);
+	public String addNewTodo(ModelMap model, Todo todo) {
+		todoService.addTodo(model.getAttribute("name").toString(), todo.getDescription(), LocalDate.now().plusMonths(8), false);
 		return "redirect:list-todos";
 	}
 
