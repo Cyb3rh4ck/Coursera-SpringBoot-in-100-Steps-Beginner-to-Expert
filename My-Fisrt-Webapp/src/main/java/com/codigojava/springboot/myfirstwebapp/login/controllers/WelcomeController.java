97b509448400bbdd4b8447e0cbe64c370cbe88ee
Cyb3rh4ck <cyb3rh4ck@outlook.com>
2025-02-20 08:06:47 -0600

    add spring security starter

--- My-Fisrt-Webapp/src/main/java/com/codigojava/springboot/myfirstwebapp/login/controllers/WelcomeController.java
+++ My-Fisrt-Webapp/src/main/java/com/codigojava/springboot/myfirstwebapp/login/controllers/WelcomeController.java
@@ -1,45 +1,21 @@
 package com.codigojava.springboot.myfirstwebapp.login.controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import com.codigojava.springboot.myfirstwebapp.login.services.AuthenticationService;
 
 @Controller
 @SessionAttributes("name")
-public class LoginController {
+public class WelcomeController {
 	
-	private Logger logger = LoggerFactory.getLogger(getClass());
-	
-	private AuthenticationService authenticationService;
-	
-	public LoginController (AuthenticationService authenticationService) {
-		this.authenticationService = authenticationService;
-	}
-	
-	@GetMapping("login")
-	public String gotologinPage() {
-		return "login";
-	}
-	
-	@PostMapping("login")
-	public String gotoWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {
-		
-		if(authenticationService.authenticate(name, password)) {
-			model.put("name", name);
-			model.put("password", password);
-			return "welcome";
-		}
-		model.put("errorMessage", "Invalid credentials! Please try again");
-		return "login";
-	}
-	
+	//private Logger logger = LoggerFactory.getLogger(getClass());
 
+	@GetMapping("/")
+	public String gotoWelcomePage(ModelMap model) {
+		model.put("name", "CodigoJava");
+		return "welcome";
+	}
 
 }
